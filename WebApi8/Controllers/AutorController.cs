56c2ac8711f8c41fb6f6e0e1ba8a24fcc3d8abc0
kewarrior <joao_pinho96@hotmail.com>
2025-07-21 15:47:36 +0100

    EndPoint - Carregar Autores

---
+++ WebApi8/Controllers/AutorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi8.Models;
+using WebApi8.Services.Autor;
+
+namespace WebApi8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+
+        private readonly IAutorInterface _autorInterface;
+        public AutorController(IAutorInterface autorInterface)
+        {
+            _autorInterface = autorInterface;
+        }
+
+        [HttpGet("ListarAutores")]
+        public async Task<ActionResult<ResponseModel<List<AutorModel>>>> ListarAutores()
+        {
+            var autores = await _autorInterface.ListarAutores();
+            return Ok(autores);
+        }
+    }
+}
---
+++ WebApi8/Services/Autor/IAutorInterface.cs
@@ -0,0 +1,11 @@
+﻿using WebApi8.Models;
+
+namespace WebApi8.Services.Autor
+{
+    public interface IAutorInterface
+    {
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<AutorModel>> BuscarAutorId(int idAutor);
+        Task<ResponseModel<AutorModel>> BuscarAutorIdLivro(int idLivro);
+    }
+}
