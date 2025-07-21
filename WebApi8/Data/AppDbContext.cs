08f22e637fe7fe93ae639575d99cba0f4f28919d
kewarrior <joao_pinho96@hotmail.com>
2025-07-21 14:31:46 +0100

    Adicionar arquivos de projeto.

---
+++ WebApi8/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base (options)
+        {  
+        }
+    }
+}
---
+++ WebApi8/Models/AutorModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi8.Models
+{
+    public class AutorModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        
+        [JsonIgnore]
+        public ICollection <LivroModel> Livros { get; set; }
+
+    }
+}
